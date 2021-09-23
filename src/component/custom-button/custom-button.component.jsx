cb9567171fc54bb70780bdc2222d70658a5be944
SureshVN <vnsuresh.abi@gmail.com>
2021-09-24 01:39:52 +0530

    Prettify the files

--- src/component/custom-button/custom-button.component.jsx
+++ src/component/custom-button/custom-button.component.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import './custom-button.styles.scss';
+import "./custom-button.styles.scss";
 
-const CustomButton = ({children, isReset, isActive, isResetPressed, ...otherProps }) => (
-	<button 
-	className={ `${isResetPressed ? 'reset-pressed' :  isReset ? 'reset-button' : ''} ${isActive ? 'stop' : ''} custom-button` }
-	{...otherProps}>
-    	{children}
-  	</button>
+const CustomButton = ({
+  children,
+  isReset,
+  isActive,
+  isResetPressed,
+  ...otherProps
+}) => (
+  <button
+    className={`${
+      isResetPressed ? "reset-pressed" : isReset ? "reset-button" : ""
+    } ${isActive ? "stop" : ""} custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
 );
 
 export default CustomButton;
--- src/component/timer/timer.component.jsx
+++ src/component/timer/timer.component.jsx
@@ -1,33 +1,36 @@
-import React from 'react'
+import React from "react";
 
-import './timer.styles.scss'
+import "./timer.styles.scss";
 
-const Timer = ({time}) => {
+const Timer = ({ time }) => {
   const hr = Math.floor((time / 3600000) % 60);
   const min = Math.floor((time / 60000) % 60);
   const sec = Math.floor((time / 1000) % 60);
-  const milliSec = ((time / 10) % 100);
+  const milliSec = (time / 10) % 100;
 
   return (
-
-	<div className="timer-container">
-    <div className='timer'>
-      { 
-        hr !== 0 && (<span className="digits hr">{hr}<small className='denoter'>h</small></span>)
-      } 
-      { 
-        (hr !== 0 || min !== 0) && (<span className="digits min">{min}<small className='denoter'>m</small></span>)
-      } 
-      <span className="digits sec">
-        {min !== 0 ? ("0" + sec).slice(-2) : sec}
-        <small className='denoter'>s</small>
-      </span>
-      <span className="digits milli-sec">
-        {("0" + milliSec).slice(-2)}
-      </span>
+    <div className="timer-container">
+      <div className="timer">
+        {hr !== 0 && (
+          <span className="digits hr">
+            {hr}
+            <small className="denoter">h</small>
+          </span>
+        )}
+        {(hr !== 0 || min !== 0) && (
+          <span className="digits min">
+            {min}
+            <small className="denoter">m</small>
+          </span>
+        )}
+        <span className="digits sec">
+          {min !== 0 ? ("0" + sec).slice(-2) : sec}
+          <small className="denoter">s</small>
+        </span>
+        <span className="digits milli-sec">{("0" + milliSec).slice(-2)}</span>
       </div>
     </div>
-)
-}
+  );
+};
 
 export default Timer;
